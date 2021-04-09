7299ec0ee01c9cf149b723bec9534b1e57d9bc43
robot-ux <robot.ux@protonmail.com>
2021-04-09 16:14:48 +0800

    fix: build cli

--- bna-next/cli/build.ts
+++ bna-next/cli/build.ts
@@ -1,16 +1,13 @@
-import { spawnSync } from 'child_process'
 import fs from 'fs'
-import { distDir } from '../utils'
+import { srcDir, distDir, conf } from '../utils'
 import path from 'path'
-
-const getDir = () => process.cwd()
+import nextBuild from 'next/dist/build'
 
 export const build = () => {
-  const dir = getDir()
-  spawnSync(`next`, [`build`, dir], { stdio: 'inherit' })
-
-  const srcDir = path.join(__dirname, '../config')
-  // copy dockerfile
-  fs.copyFileSync(`${srcDir}/dockerignore`, `${distDir}/.dockerignore`)
-  fs.copyFileSync(`${srcDir}/Dockerfile`, `${distDir}/Dockerfile`)
+  nextBuild(srcDir, conf as any).then(() => {
+    const srcDir = path.join(__dirname, '../config')
+    // copy dockerfile
+    fs.copyFileSync(`${srcDir}/dockerignore`, `${distDir}/.dockerignore`)
+    fs.copyFileSync(`${srcDir}/Dockerfile`, `${distDir}/Dockerfile`)
+  })
 }

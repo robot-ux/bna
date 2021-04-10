e260e2e9926ab967fe7b994ae3a529cb072dbd6e
robot-ux <robot.ux@protonmail.com>
2021-04-10 20:00:19 +0800

    fix: dynamicAssetPrefix file extension and add App.tsx

--- bna-next/components/dynamicAssetPrefix.tsx
+++ bna-next/components/dynamicAssetPrefix.tsx
@@ -1,15 +1,4 @@
-import { IncomingMessage } from 'http'
 import React from 'react'
-
-export const getAssetPrefixByReq = (req: IncomingMessage) => {
-  const countryCode = req.headers['cloudfront-viewer-country']
-  const _cdnHost =
-    (countryCode === 'CN'
-      ? process.env.NEXT_PUBLIC_STATIC_HOST_CN
-      : process.env.NEXT_PUBLIC_STATIC_HOST) || ''
-
-  return _cdnHost
-}
 
 const AssetPreifxContext = React.createContext('')
 
@@ -21,7 +10,7 @@ export const AssetPrefixProvider = ({
   value: string
 }) => {
   const Provider = AssetPreifxContext.Provider
-  return <Provider value={value}>{children}</Provider>
+  return <Provider value={value || ''}>{children}</Provider>
 }
 
 export const useAssetPrefix = () => {
--- bna-next/components/index.ts
+++ bna-next/components/index.ts
@@ -1,4 +1,5 @@
 export * from './document'
+export * from './App'
 export * from './Gtag'
 export * from './Ga'
 export * from './dynamicAssetPrefix'

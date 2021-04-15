d06cbc1d9157f18c6e138ff2f9a0425f09930eb1
robot-ux <robot.ux@protonmail.com>
2021-04-15 19:04:00 +0800

    feat: remove dynamic asset prefix

--- bna-next/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './document'
-export * from './App'
-export * from './Gtag'
-export * from './Ga'
-export * from './dynamicAssetPrefix'
--- bna-next/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-
-import conf from '../config/next.config'
-import pkg from '../package.json'
-
-const srcDir = process.cwd()
-const distDir = path.join(srcDir, '.next')
-
-export { conf, pkg, srcDir, distDir }
